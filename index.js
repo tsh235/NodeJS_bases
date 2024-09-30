bdef5caf6d86c154354135c91b862a8eb5b607c9
tsh235 <tanyashustrova@yandex.ru>
2024-10-01 00:42:24 +0300

    hw esson 1

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hello, Node.js');
+setImmediate(() => {
+  console.log('Был диван,');
+});
+
+setTimeout(() => {
+  console.log('Выйди вон!');
+}, 100);
+
+setImmediate(() => {
+  console.log('На диване');
+});
+
+process.nextTick(() => {
+  console.log('Чемодан,');
+});
+
+setTimeout(() => {
+  console.log('Кто не верит –');
+}, 10);
+
+setImmediate(() => {
+  console.log('Ехал слон.');
+});
+
+process.nextTick(() => {
+  console.log('В чемодане');
+});
+
+console.log('Плыл по морю');
