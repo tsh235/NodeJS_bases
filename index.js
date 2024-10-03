5ace8dca181813732833e0b9955172e8a1d8eaf4
tsh235 <tanyashustrova@yandex.ru>
2024-10-03 12:45:45 +0300

    lesson 6 task 2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello, Node.js');
+import { createReadStream, createWriteStream } from 'node:fs';
+import { readdir } from 'node:fs/promises';
+
+const mergingTxtFiles = async (sourceDir, outputFile) => {
+	const wStream = createWriteStream(outputFile);
+
+	try {
+		const files = await readdir(sourceDir);
+
+		files.forEach(file => {
+			const ext = file.split('.').pop();
+
+			if (ext === 'txt') {
+				const rStream = createReadStream(`${sourceDir}/${file}`);
+
+        rStream.on('data', chunk => {
+          wStream.write(`[${file.split('.')[0]}]\n${chunk}\n`);
+        });
+			}
+		});
+
+		console.log(`Файл ${outputFile} успешно записан!`);
+	} catch (err) {
+		console.log(`Ошибка: ${err}`);
+	}
+};
+
+mergingTxtFiles('./files', 'result.txt');
